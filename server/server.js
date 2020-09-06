059b3881a444a957b12f95b48790c8fb2e77e93d
Jack Hu <jackhu242@gmail.com>
2020-09-06 12:44:14 -0600

    lint fixes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const server = express();
-const port = process.env.SERVER_PORT;
+
+const app = express();
+const port = 3001;
 
 app.get('/', (req, res) => {
-  res.send('connected to gg backend')
-})
+  res.send('connected to gg backend');
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
